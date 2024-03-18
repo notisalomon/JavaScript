c86075058f1ec0b6e873e49bc7bd047d0684f08b
notisalomon <sulejmani.labinot@gmail.com>
2024-03-18 21:50:13 +0100

    Create weather-app.js

---
+++ weather-app.js
@@ -0,0 +1,21 @@
+console.log('Hello World')
+console.log('Labinot')
+
+// variables
+name = 'Peter Pan'
+console.log(name)
+
+sentence = 'how are you doing today, nice to see you, hope you have a great day!'
+console.log(sentence)
+
+// operators
+// fruit = prompt('what is your favorite fruit?')
+// console.log(fruit)
+
+food = prompt('how much was the food?')
+tipPercentage = prompt('tip %?')
+tipAmount = food * tipPercentage
+
+console.log(tipAmount)
+
+// user input
