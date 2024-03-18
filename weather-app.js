e712df2caea38fa5c4f9b0bdb4a5b1a48b96ad02
notisalomon <sulejmani.labinot@gmail.com>
2024-03-18 21:52:09 +0100

    Update weather-app.js

--- weather-app.js
+++ weather-app.js
@@ -2,20 +2,69 @@ console.log('Hello World')
 console.log('Labinot')
 
 // variables
-name = 'Peter Pan'
+let name = 'Peter Pan'
 console.log(name)
 
-sentence = 'how are you doing today, nice to see you, hope you have a great day!'
+let sentence = 'how are you doing today, nice to see you, hope you have a great day!'
 console.log(sentence)
+
+
 
 // operators
 // fruit = prompt('what is your favorite fruit?')
 // console.log(fruit)
 
-food = prompt('how much was the food?')
-tipPercentage = prompt('tip %?')
-tipAmount = food * tipPercentage
+// * / ** + -
 
-console.log(tipAmount)
+// food = Number(prompt('how much was the food?'))
+// tipPercentage = Number(prompt('tip %?') / 100)
+// tipAmount = food * tipPercentage
+// total = food + tipAmount
+
+// console.log('Tip Amount:', tipAmount)
+// console.log('Total:', total)
+// alert(tipAmount)
 
 // user input
+
+/* data types (strings, numbers)
+numbers = 1, 5, 10, 100, 2.5
+strings = 'hello', "what is up"
+arrays = []
+objects = {}
+boolean = true / false (banks)
+*/
+
+/* Math Operators
+Multiply *
+Divide /
+Exponents **
+Modulo & Remainder % 5%2 = 1
+Add +
+Subtract -
+*/
+
+/*
+Math Methods
+Floor - Rounds down
+Ceil - Rounds up
+Random - gives you number between 0 and 1
+*/
+
+// Baby weather app (conditionals)
+// if rain then 'Grab your umbrella emoji'
+// else 'Wear your sunglasses emoji'
+let weather = prompt('What is the weather like?')
+if (weather == 'rainy') {
+  console.log('Grab your umberella ☂️')
+}
+else {
+  console.log('Wear your sunglasses 😎')
+}
+
+// conditional operators
+// ==, ===, >, <, <=, >=, !=, !==
+
+// Functions
+
+// Type casting
